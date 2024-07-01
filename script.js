430febe594e3bf53c217a6cce6786a2f9c103e42
gitongalarson <larsonmwendah07@gmail.com>
2024-07-01 15:27:04 +0300

    latest 2

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
+function getStudentMark() {
+    let marks;
+    while (true) {
+        marks = prompt("Input Student Marks (0-100):");
+        marks = parseFloat(marks);
+        if (!isNaN(marks) && marks >= 0 && marks <= 100) {
+            break;
+        } else {
+            alert("Kindly enter a number between 0 and 100.");
+        }
+    }
 
-function getStudentMark(){
-let marks;
-if (true);
-marks = prompt("Input Student Marks (0-100):");
-marks = parseFloat(Marks);
-if(!isNaN(marks) && marks >=0 && marks <=100){
- break;
-} else {
-    alert("KIndly enter a number between 0 and 100.");
- }
+    let grade;
+    if (marks > 79) {
+        grade = 'A';
+    } else if (marks >= 60) {
+        grade = 'B';
+    } else if (marks >= 50) {
+        grade = 'C';
+    } else if (marks >= 40) {
+        grade = 'D';
+    } else {
+        grade = 'E';
+    }
 
- }
-let grade;
-if (marks > 79) {
-    grade ='A'
-} else if (marks >=60){
-    grade = 'B'
-} else if (marks >=50){
-    grade = 'C'
-} else if (marks >=40) {
-    grade ='D'
-} else {
-    grade = 'E'
+    alert(`Final grade for ${marks} is ${grade}`);
 }
 
-
-alert(`Final grade for ${marks} is ${grade}`);
-
-getStudentMark()
+getStudentMark();
